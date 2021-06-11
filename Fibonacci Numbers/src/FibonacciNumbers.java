451a622c5f03fdb0ec116e42ffb9edede31ef728
KHALID HASNAOUI <67362180+hasnaouk@users.noreply.github.com>
2021-06-11 10:52:52 -0400

    commit: 1st draft code

---
+++ Fibonacci Numbers/src/FibonacciNumbers.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class FibonacciNumbers {
+	
+	// method that gives the nth Fibonacci Number
+	public static long FibNum(long n) {		
+		if (n==0) // base case
+			return 0;
+		else if (n==1) // base case
+			return 1;
+		else
+			return FibNum(n-2) + FibNum(n-1); // reduction and recursive calls
+	}
+
+	public static void main(String[] args) {		
+		Scanner input = new Scanner(System.in);		
+		// enter an index for the Fibonacci Number		
+		System.out.print("Enter an index for the Fibonacci number: ");
+		 int index = input.nextInt();
+		
+		 // find and display the Fibonacci number
+		 System.out.println("The Fibonacci number at index " + index + " is " + FibNum(index));
+		
+	}
+
+}
